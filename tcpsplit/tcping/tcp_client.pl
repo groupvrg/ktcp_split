f168bb7f3fac221558ece8a4d79573d483e38be0
Markuze Alex <amarkuze@vmware.com>
2017-10-25 16:35:32 +0300

    tcping

---
+++ tcpsplit/tcping/tcp_client.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl -w
+
+use strict;
+use autodie;
+use IO::Socket;
+use Time::HiRes;
+
+$| = 1; #?
+sub parse_config {
+	die "sorry: parse_config not implemented yet...\n";
+}
+
+sub get_params {
+	if ( -r "./config" ) {
+		return parse_config();
+	}
+}
+
+my $serverip = '35.189.112.223';	#VMA
+$serverip='35.189.77.30';
+#my $serverip = '10.154.0.6';
+#my $serverip = 'localhost';
+my $serverport = 5556;
+my $size = (64 * 1024) - 28;
+get_params();
+
+my $time = Time::HiRes::gettimeofday();
+my $message = IO::Socket::INET->new(Proto=>"tcp", PeerPort=>$serverport,
+				     PeerAddr=>$serverip);
+$time = Time::HiRes::gettimeofday() - $time;
+printf "connected...%f\n", $time;
+$time = Time::HiRes::gettimeofday();
+$message->send("Ping!");
+print "Ping sent...\n";
+my $data = "";
+$message->recv($data, $size);
+$time = Time::HiRes::gettimeofday() - $time;
+printf ">$data<\n%f\n", $time;
+$message->close();
---
+++ tcpsplit/tcping/tcp_server.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl -w
+
+use strict;
+use autodie;
+use IO::Socket;
+
+sub parse_config {
+	die "sorry: parse_config not implemented yet...\n";
+}
+
+sub get_params {
+	if ( -r "./config" ) {
+		return parse_config();
+	}
+}
+
+my $serverport = 5556;
+my $size = (64 * 1024) - 28;
+get_params();
+
+my $server = IO::Socket::INET->new(LocalPort=>$serverport, Proto=>"tcp",
+				   Listen=>5, Reuse=>1);
+
+my ($datagram, $flags);
+
+#while ($server->recv($datagram, $size, $flags)) {
+#	my $ipaddr = $server->peerhost;
+#	print "received ", length($datagram), "bytes from $ipaddr\n";
+#}
+while (1) {
+	my $client = $server->accept();
+
+	#get information about a newly connected client
+
+	my $client_address = $client->peerhost();
+	my $client_port = $client->peerport();
+	print "connection from $client_address:$client_port\n";
+
+	#read up to 1024 characters from the connected client
+	my $data = "";
+	$client->recv($data, $size);
+	print "received data: $data\n";
+
+	# write response data to the connected client
+	$data = "ok";
+	$client->send($data);
+
+	# notify client that response has been sent
+	shutdown($client, 1);
+}
+$server->close();
