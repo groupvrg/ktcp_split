3f1b5860bc71cfe8d908957f7b1c5577f173a002
Aran Bergman <aran.bergman@gmail.com>
2018-02-15 15:49:46 +0200

    remove test.sh, replaced by install.sh

--- README.md
+++
@@ -1,7 +0,0 @@
-# Instructions
-
-To add a listening server, write
-`echo <tid>,<port> > /proc/cbn/cbn_proc`
-
-For example, to listen on port 5005 with mark = 10 do
-`echo 10,5005 > /proc/cbn/cbn_proc`
---
+++ tcpsplit/install.sh
@@ -0,0 +1,9 @@
+sudo rmmod cbn_split
+make
+if [ "$?" != 0 ]; then
+	exit
+fi
+sudo insmod cbn_split.ko
+sudo sh -c 'echo 0 > /proc/sys/kernel/hung_task_timeout_secs'
+sudo sh -c 'echo 1 > /proc/sys/kernel/ftrace_dump_on_oops'
+echo 10,12345 > /proc/cbn/cbn_proc
