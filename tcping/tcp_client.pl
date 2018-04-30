10b4810b48ed69dcf4c9c2ae29816472642c074e
Markuze Alex <amarkuze@vmware.com>
2018-04-30 12:13:45 +0000

    tcp_client: parse .config file

--- tcping/tcp_client.pl
+++ tcping/tcp_client.pl
@@ -4,10 +4,49 @@ use strict;
 use autodie;
 use IO::Socket;
 use Time::HiRes;
-
+use Regexp::Common qw/ net number /;
 $| = 1; #?
+
+my $serverip = '127.0.0.1';
+my $serverport = 5556;
+my $size = 1024;
+
+sub parse_size {
+	my $base = shift;
+	my $str = shift;
+	my $mult = 1;
+
+	if (defined $str) {
+		$mult = 1000 if ($str eq 'k');
+		$mult = 1024 if ($str eq 'K');
+		$mult = (1000 * 1000) if ($str eq 'm');
+		$mult = (1024 * 1024) if ($str eq 'M');
+	}
+	return $base * $mult;
+}
+
+sub parse_line {
+	my $line = shift;
+	if ($line =~ /server\s+($RE{net}{IPv4})/) {
+		$serverip = $1 if defined ($1);
+	}
+
+	if ($line =~ /port\s+(\d{1,5})/) {
+		$serverport = $1 if defined ($1);
+	}
+
+	if ($line =~ /size\s+(\d+)([kKmM]*)/) {
+		my $base = $1 if defined ($1);
+		my $str = $2 if defined ($2);
+		$size = parse_size($base, $str) if defined $base;
+	}
+}
+
 sub parse_config {
-	die "sorry: parse_config not implemented yet...\n";
+	open (my $fh, "<", './config');
+	while (<$fh>) {
+		parse_line($_);
+	}
 }
 
 sub get_params {
@@ -16,11 +55,8 @@ sub get_params {
 	}
 }
 
-my $serverip = '10.128.0.9';	#VMA
-#my $serverip = '35.192.100.86';	#VMA
-my $serverport = 5556;
-my $size = (64 * 1024) - 28;
 get_params();
+printf "$serverip $serverport $size\n";
 
 my $time = Time::HiRes::gettimeofday();
 my $message = IO::Socket::INET->new(Proto=>"tcp", PeerPort=>$serverport,
