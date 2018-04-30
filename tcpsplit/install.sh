3d1dbd6c49ac14dd4c8678b296312059e7df3cf3
Markuze Alex <amarkuze@vmware.com>
2018-04-30 13:43:46 +0000

    local config change

--- tcpsplit/install.sh
+++ tcpsplit/install.sh
@@ -6,4 +6,4 @@ fi
 sudo insmod cbn_split.ko
 sudo sh -c 'echo 0 > /proc/sys/kernel/hung_task_timeout_secs'
 sudo sh -c 'echo 1 > /proc/sys/kernel/ftrace_dump_on_oops'
-#echo 10,12345 > /proc/cbn/cbn_proc
+echo 2,9216 > /proc/cbn/cbn_proc
--- tcpsplit/setup.sh
+++ tcpsplit/setup.sh
@@ -1,10 +1,1 @@
-#sudo ip link add name pipe type ipip remote 10.0.2.4 local 10.0.2.15
-#sudo ip addr add 10.0.2.15 dev pipe
-#sudo ip link set up dev pipe
-sudo iptables -I PREROUTING -i ipip_a -t raw -j MARK --set-mark 10
-sudo iptables -I PREROUTING -i ipip_b -t raw -j MARK --set-mark 10
-sudo iptables -I PREROUTING -t nat  -p tcp -m mark --mark 10 -j REDIRECT --to-port 9216
-#sudo iptables -I PREROUTING -i pipe -t nat  -p tcp -j REDIRECT --to-port 9216
-#sudo iptables -I PREROUTING -t nat  -p tcp -j REDIRECT --to-port 9216
-sudo sysctl -w net.ipv4.ip_forward=1
-sudo sh -c "echo 0 > /proc/sys/net/ipv4/conf/all/rp_filter"
+sudo iptables -I PREROUTING -t nat  -p tcp -m mark --mark 2 -j REDIRECT --to-port 9216
