265f3003896a2f726157510a5d7895957b4f4f20
Build VM <build@example.com>
2019-06-02 18:17:42 +0000

    adding version stamping

--- tcpsplit/install.sh
+++ tcpsplit/install.sh
@@ -1,5 +1,8 @@
-#sudo rmmod cbn_split
+
+sed -i "s/KTCP_VERSION/`git rev-parse --short HEAD`/g" split.c
+
 make
+git checkout split.c
 if [ "$?" != 0 ]; then
 	RED='\033[1;31m'
 	NC='\033[0m' # No Color
@@ -10,10 +13,10 @@ if [ "$?" != 0 ]; then
 fi
 
 if [ -n "$1" ]; then
-	sudo insmod cbn_split.ko pool_size=$1
-else
-	sudo insmod cbn_split.ko
+	pool_size=$1
 fi
+exit
+sudo insmod cbn_split.ko pool_size=$pool_size
 
 sudo sh -c 'echo 0 > /proc/sys/kernel/hung_task_timeout_secs'
 sudo sh -c 'echo 1 > /proc/sys/kernel/ftrace_dump_on_oops'
