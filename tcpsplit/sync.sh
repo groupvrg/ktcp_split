0b85c70a3f787bdd0a162daf6e02b28c6671c8b3
Build VM <build@example.com>
2019-06-04 17:22:57 +0000

    dumb sync.sh to weird shit on GCP

--- tcpsplit/sync.sh
+++ tcpsplit/sync.sh
@@ -3,7 +3,8 @@ links=(ew2b-mark-mc-2 ew2b-mark-mc-1)
 for link in ${links[@]}; do
 	echo $link
 	#	rsync -re ssh -i ~/CBN_GCP_MASTER_KEY . $link:/ktcp_split/tcpsplit/
-	ssh -i ~/CBN_GCP_MASTER_KEY $link rm -f `pwd`/*
+	ssh -i ~/CBN_GCP_MASTER_KEY $link rm -rf `pwd`/*
+	ssh -i ~/CBN_GCP_MASTER_KEY $link rm -f .*
 	rsync -vrue "ssh -i ~/CBN_GCP_MASTER_KEY " . $link:`pwd` &
 done
 wait
