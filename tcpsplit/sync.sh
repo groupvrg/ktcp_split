ad41094503c72236c3ce7948a78f7f879003d203
Build VM <build@example.com>
2019-06-05 15:40:54 +0000

    sync for new setup

--- tcpsplit/sync.sh
+++ tcpsplit/sync.sh
@@ -1,10 +1,10 @@
-links=(ew2b-mark-mc-2 ew2b-mark-mc-1)
+links=(euw2b-mark-mc-3 ew2b-mark-mc-4)
 
 for link in ${links[@]}; do
 	echo $link
 	#	rsync -re ssh -i ~/CBN_GCP_MASTER_KEY . $link:/ktcp_split/tcpsplit/
-	ssh -i ~/CBN_GCP_MASTER_KEY $link rm -rf `pwd`/*
-	ssh -i ~/CBN_GCP_MASTER_KEY $link rm -f .*
+	#ssh -i ~/CBN_GCP_MASTER_KEY $link rm -rf `pwd`/*
+	#ssh -i ~/CBN_GCP_MASTER_KEY $link rm -f .*
 	rsync -vrue "ssh -i ~/CBN_GCP_MASTER_KEY " . $link:`pwd` &
 done
 wait
