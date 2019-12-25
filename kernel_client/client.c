0f8d507be57b82172f1ed3dd8d273486abfd860b
Build VM <build@example.com>
2019-12-25 16:22:47 +0000

    exporting thread_poll api

--- kernel_client/client.c
+++ kernel_client/client.c
@@ -29,6 +29,7 @@ typedef void (*bind_mask_func)(struct task_struct *, const struct cpumask *);
 bind_mask_func pkthread_bind_mask;
 
 static struct proc_dir_entry *proc_dir;
+static struct kthread_pool thread_pool = {.pool_size = 128 };
 static struct task_struct *udp_client_task;
 static struct task_struct *tcp_client_task;
 
@@ -79,18 +80,118 @@ static int noop_open(struct inode *inode, struct file *file)
 	return 0;
 }
 
+/*
+	TODO: Need a thread to do accept (look at ktcp)
+	TODO: Create a thread pool user...
+	TODO: integrate DAMN, KTCP magazines
+*/
+static void tcp_zcopy_rx(struct socket *sock, struct page **pages_array, unsigned int nr_pages)
+{
+	struct sock *sk = sock->sk;
+	const skb_frag_t *frags;
+	u32 seq, len, offset, nr = 0;
+	struct tcp_sock *tp;
+	struct sk_buff *skb;
+	int rc = -ENOTCONN;
+
+	lock_sock(sk);
+
+	if (sk->sk_state == TCP_LISTEN)
+		goto out;
+
+	sock_rps_record_flow(sk);
+
+	tp = tcp_sk(sk);
+	seq = tp->copied_seq;
+#if 0
+	unsigned long size = nr_pages << PAGE_SHIFT;
+
+	/* We dont actually care, accept everything...*/
+	if (tcp_inq(sk) < size) {
+		ret = sock_flag(sk, SOCK_DONE) ? -EIO : -EAGAIN;
+		goto out;
+	}
+	/* Abort if urgent data is in the area  --- Hmm.....*/
+	if (unlikely(tp->urg_data)) {
+		u32 urg_offset = tp->urg_seq - seq;
+
+		ret = -EINVAL;
+		if (urg_offset < size)
+			goto out;
+	}
+#endif
+	skb = tcp_recv_skb(sk, seq, &offset);
+	ret = -EINVAL;
+skb_start:
+	offset -= skb_headlen(skb);
+	/* Linear data present... - Handle it or Fix virtio */
+	if ((int)offset < 0)
+		goto out;
+	/* frag list present ? eehnmmm... gro wtf?*/
+	if (skb_has_frag_list(skb))
+		goto out;
+	len = skb->data_len - offset;
+	frags = skb_shinfo(skb)->frags;
+	while (offset) {
+		if (frags->size > offset)
+			goto out;
+		offset -= frags->size;
+		frags++;
+	}
+	while (nr < nr_pages) {
+		if (len) {
+			if (len < PAGE_SIZE)
+				goto out;
+			if (frags->size != PAGE_SIZE || frags->page_offset)
+				goto out;
+			pages_array[nr++] = skb_frag_page(frags);
+			frags++;
+			len -= PAGE_SIZE;
+			seq += PAGE_SIZE;
+			continue;
+		}
+		skb = skb->next;
+		offset = seq - TCP_SKB_CB(skb)->seq;
+		goto skb_start;
+	}
+#if 0
+	/* Ok now we need to get these pages...*/
+	for (nr = 0; nr < nr_pages; nr++) {
+		ret = vm_insert_page(vma, vma->vm_start + (nr << PAGE_SHIFT),
+				     pages_array[nr]);
+		if (ret)
+			goto out;
+	}
+#endif
+	/* operation is complete, we can 'consume' all skbs */
+	tp->copied_seq = seq;
+	tcp_rcv_space_adjust(sk);
+
+	/* Clean up data we have read: This will do ACK frames. */
+	tcp_recv_skb(sk, seq, &offset);
+	tcp_cleanup_rbuf(sk, size);
+
+	ret = 0;
+out:
+	release_sock(sk);
+
+	return ret;
+}
+
 static inline void tcp_client(void)
 {
 #define PORT	8080
 //#define SERVER_ADDR (10<<24|1<<16|4<<8|38) /*10.1.4.38*/
 #define SERVER_ADDR (10<<24|154<<16|0<<8|21) /*10.154.0.21*/
 	int rc, i = 0, j;
+	unsigned long cnt, max;
 	struct socket *tx = NULL;
 	struct sockaddr_in srv_addr = {0};
 	struct msghdr msg = { 0 };
 	struct kvec kvec[16];
 	void *base = NULL;
 
+	cnt = max = 0;
         srv_addr.sin_family             = AF_INET;
         srv_addr.sin_addr.s_addr        = htonl(SERVER_ADDR);
         srv_addr.sin_port               = htons(PORT);
@@ -103,31 +204,42 @@ static inline void tcp_client(void)
 		rc = -ENOMEM;
 		goto err;
 	}
-
+ #define virt_to_pfn(kaddr) (__pa(kaddr) >> PAGE_SHIFT)
 	for (i = 0; i < 16; i++) {
 		kvec[i].iov_len = PAGE_SIZE;
 		kvec[i].iov_base = base + (i * PAGE_SIZE);
+		trace_printk("Page %d) 0x%lx (%lu)\n",i, (unsigned long)(kvec[i].iov_base), virt_to_pfn(kvec[i].iov_base));
 	}
 
 	if ((rc = sock_create_kern(&init_net, PF_INET, SOCK_STREAM, IPPROTO_TCP, &tx))) {
-                trace_printk("RC = %d (%d)", rc, __LINE__);
+                trace_printk("RC = %d (%d)\n", rc, __LINE__);
 		goto err;
         }
 
 	sock_set_flag(tx->sk, SOCK_KERN_ZEROCOPY);
 
         if ((rc = kernel_connect(tx, (struct sockaddr *)&srv_addr, sizeof(struct sockaddr), 0))) {
-                trace_printk("RC = %d (%d)", rc, __LINE__);
+                trace_printk("RC = %d (%d)\n", rc, __LINE__);
 		goto err;
         }
 	trace_printk("Connected, sending...\n");
 
 	for (i = 0; i < (1<<19); i++) {
-		for (j = 0; j < 16; j++) {
-			get_page(virt_to_page(base + (i * PAGE_SIZE)));
-		}
+	//	for (j = 0; j < 16; j++) {
+	//		get_page(virt_to_page(base + (i * PAGE_SIZE)));
+	//	}
 
-		kernel_sendmsg(tx, &msg, kvec, 16, (16 << PAGE_SHIFT));
+
+		while ((rc = kernel_sendmsg(tx, &msg, kvec, 16, (16 << PAGE_SHIFT))) <= 0) {
+			//trace_printk("Received an Err %d\n", rc);
+			rc = 0;
+			cnt++;
+		}
+		if (unlikely(cnt > max)) {
+			trace_printk("Polling for %lu [%lu]\n", cnt, max);
+			max = cnt;
+		}
+		cnt = 0;
 	}
 	trace_printk("Hello messages sent.(%d)\n", i);
 	goto ok;
@@ -202,6 +314,7 @@ static const struct file_operations client_fops = {
 
 static __init int client_init(void)
 {
+	cbn_kthread_pool_init(&thread_pool);
 	proc_dir = proc_mkdir_mode(POLLER_DIR_NAME, 00555, NULL);
 	pkthread_bind_mask = (void *)kallsyms_lookup_name("kthread_bind_mask");
 
@@ -215,6 +328,8 @@ static __init int client_init(void)
 
 	wake_up_process(udp_client_task);
 	wake_up_process(tcp_client_task);
+
+	kthread_pool_run(&thread_pool, split_server, uint2void(tid, port));
 
 	if (!proc_create_data("udp_"procname, 0666, proc_dir, &client_fops, udp_client_task))
 		goto err;
