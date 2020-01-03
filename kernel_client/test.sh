853c813df81d3effb917ccdf12dd3fefec6d594a
Build VM <build@example.com>
2020-01-03 19:36:53 +0000

    Adding patches

--- kernel_client/test.sh
+++ kernel_client/test.sh
@@ -6,6 +6,6 @@ sudo insmod client.ko
 #echo trace_sendmsg >  set_graph_function
 #echo function_graph > current_tracer
 sudo sh -c "echo 16 > /sys/kernel/debug/tracing/buffer_size_kb"
-sudo sh -c "echo trace_sendmsg > /sys/kernel/debug/tracing/set_graph_function"
-sudo sh -c "echo function_graph > /sys/kernel/debug/tracing/current_tracer"
+#sudo sh -c "echo trace_sendmsg > /sys/kernel/debug/tracing/set_graph_function"
+#sudo sh -c "echo function_graph > /sys/kernel/debug/tracing/current_tracer"
 echo 0 > /proc/io_client/tcp_client
